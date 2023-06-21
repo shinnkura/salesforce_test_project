79dc0e1c06015006ac296a7302116c01f3b59377
Shinzo Nakaoka <shinzonakaoka@ShinzonoMacBook-Air.local>
2023-06-21 16:13:05 +0900

    リスト表示

--- force-app/main/default/lwc/helloLwc/helloLwc.js
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -1,8 +1,32 @@
-import { LightningElement , track } from 'lwc';
+// import { LightningElement , track } from 'lwc';
+
+// export default class HelloLwc extends LightningElement {
+//     // @track name = 'Lightning Web Components';
+//     // onNameChanged() {
+//     //     this.name = this.template.querySelector('input').value;
+//     // }
+//     @track isChecked = false;
+//     onCheckboxChanged() {
+//         this.isChecked = this.template.querySelector('input').checked;
+//     }
+// }
+
+
+import { LightningElement, track } from 'lwc';
 
 export default class HelloLwc extends LightningElement {
-    @track name = 'Lightning Web Components';
-    onNameChanged() {
-        this.name = this.template.querySelector('input').value;
-    }
+    @track members = [
+        {
+            id: 1,
+            name: 'sato',
+        },
+        {
+            id: 2,
+            name: 'suzuki',
+        },
+        {
+            id: 3,
+            name: 'tanaka',
+        },
+    ];
 }
